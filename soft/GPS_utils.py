4ca0ab66a543b1da0d31176c688e62b98768e47b
Suzanne Aigrain <suzanne.aigrain@gmail.com>
2017-11-01 17:23:33 +0000

    Dan's CG trick partially implemented - not working correctly yet

--- soft/GPS_utils.py
+++ soft/GPS_utils.py
@@ -2,6 +2,7 @@ import numpy as np
 import matplotlib.pyplot as plt
 from scipy.optimize import minimize
 from celerite import terms, GP
+from scipy.sparse.linalg import LinearOperator, cg
 import emcee, corner
 import sys
 import matplotlib.gridspec as gridspec
@@ -9,12 +10,16 @@ import scipy.io.matlab as siom
 
 SPEED_OF_LIGHT = 2.99796458e8
 
+####################################################################################
+# Routines for fitting one spectrum at a time                                      #
+####################################################################################
+
 def NLL0(p, gp, y):
     gp.set_parameter_vector(p)
     g = gp.grad_log_likelihood(y, quiet=True)
     return -g[0], -g[1]
 
-def Fit0(x, y, yerr = None, verbose = True, doPlot = False, \
+def Fit0(x, y, yerr, verbose = True, doPlot = False, \
              xpred = None):
     k = terms.Matern32Term(log_sigma = 0.0, log_rho = 0.0)
     gp = GP(k, mean = 1.0)
@@ -34,67 +39,20 @@ def Fit0(x, y, yerr = None, verbose = True, doPlot = False, \
         plt.fill_between(xpred, mu + std, mu - std, 'C0', alpha = 0.4, lw = 0)
     return soln.x, mu, std
 
-def pairwise_ccf(spec, maxlags = None):
-    K,N = spec.shape
-    lags = np.arange(-N+1,N)
-    ccfs = np.zeros((K,K,2*N-1))
-    for i in np.arange(K):
-        x = spec[i,:].flatten()
-        for j in np.arange(K):
-            if j > i:
-                continue
-            print i, j
-            y = spec[j,:].flatten()
-            ccfs[i,j,:] = np.correlate(x, y, 'full') / np.sqrt(np.dot(x, x) * np.dot(y, y))
-    if maxlags:
-        l = abs(lags) <= maxlags
-        lags = lags[l]
-        ccfs = ccfs[:,:,l]
-    return lags, ccfs
-
-def Wolkswagen(x2d, y2d, y2derr, truevel, doLog = True, sampling = 100, wav2si = 1.0e-10):
-    K, N = x2d.shape
-    if doLog:
-        xang = x2d * wav2si
-        log_xpred = np.linspace(np.log(xang.min()), np.log(xang.max()), N*sampling)
-        xpred = np.exp(log_xpred) / wav2si
-        dlw = log_xpred[1]-log_xpred[0]
-    else:
-        xpred = np.linspace(x2d.min(), x2d.max(), N*sampling)
-        dw = xpred[1]-xpred[0]
-    HPs = np.zeros((K,2))
-    ypred = np.zeros((K,len(xpred)))
-    plt.figure(1)
-    plt.clf()
-    for i in range(K):
-        x = x2d[i,:].flatten()
-        y = y2d[i,:].flatten()
-        yerr = y2derr[i,:].flatten()
-        plt.errorbar(x, y - i, yerr = yerr, fmt = 'k.', capsize = 0, alpha=0.5)
-        res = Fit0(x, y, yerr, verbose = False, xpred = xpred)
-        HPs[i,:] = res[0]
-        ypred[i,:] = res[1]
-        plt.plot(xpred, res[1] - i, color='C0')
-        plt.fill_between(xpred, res[1] + res[2] - i, res[1] - res[2] - i, \
-                             color = 'C0', alpha=0.4, lw=0)
-    lags, ccfs = pairwise_ccf(ypred, 500)
-    dvel = lags * dlw * 2.99796458e8 / 1e3
-    plt.figure(2)
-    plt.clf()
-    for i in range(K):
-        for j in range(K):
-            if j > i: continue
-            plt.clf()
-            plt.plot(dvel, ccfs[i,j,:].flatten())
-            ipk = np.argmax(ccfs[i,j,:].flatten())
-            plt.axvline(dvel[ipk])
-            print i,j, dvel[ipk], (truevel[j]-truevel[i])/1e3
-            plt.draw()
-            raw_input()
+def Fit0_Jitter(x, y, yerr, verbose = True):
+    k = terms.Matern32Term(log_sigma = 0.0, log_rho = 0.0)
+    k += terms.JitterTerm(log_sigma = np.log(np.median(yerr)))
+    gp = GP(k, mean = 1.0)
+    gp.compute(x)
+    soln = minimize(NLL0, gp.get_parameter_vector(), jac=True, args=(gp,y))
+    gp.set_parameter_vector(soln.x)
+    if verbose:
+        print 'Initial pars:', HP_init
+        print 'Fitted pars:', soln.x
+    return soln.x
 
 ####################################################################################
-# The CCF as implemented by correlate is not quite right, even as normalised here. #
-# Probably best to focus on getting Rolls Royce working though next                #
+# Single component case - using celerite out of the box                            #
 ####################################################################################
 
 def LP1(p, gp, x2d, y2d, y2derr):
@@ -156,7 +114,7 @@ def Pred1_2D(par, x2d, y2d, y2derr, doPlot = True, x2dpred = None):
                                  y2dpred[i,:] - y2dprederr[i,:] - i, color = 'C0', alpha = 0.4, lw = 0)
     return x2dpred, y2dpred, y2dprederr
 
-def RollsRoyce_1Comp(wav, flux, flux_err, nsteps = 2000, nrange = 3, prefix = 'RR1'):
+def GPSpec_1Comp(wav, flux, flux_err, nsteps = 2000, nrange = 3, prefix = 'RR1'):
     # NB: input wavelengths should be in nm, flux continuum should be about 1
     K, N = wav.shape
     # Create 2-D array of scaled log wavelengths for fitting
@@ -336,7 +294,7 @@ def RollsRoyce_1Comp(wav, flux, flux_err, nsteps = 2000, nrange = 3, prefix = 'R
     plt.savefig('%s_combined.png' % prefix)
     return par_MAP, par50, par50-par16, par84-par50, [fig1, fig2, fig3, fig4]
     
-def test():
+def test1():
     plt.close('all')
     d = siom.loadmat('/Users/aigrain/Soft/GPSpec/data/synth_dataset_002.mat')
     wav = d['wavelength'] * 0.1
@@ -346,5 +304,344 @@ def test():
     starvel = d['starvel'].flatten()
     true_shifts = (baryvel[1:] + starvel[1:]) - (baryvel[0] + starvel[0])
     print true_shifts * 1e-3
-    figs = RollsRoyce_1Comp(wav, flux, flux_err, nsteps = 2000, prefix = 'synth2')
+    figs = GPSpec_1Comp(wav, flux, flux_err, nsteps = 2000, prefix = 'synth2')
     return
+
+####################################################################################
+# Two component case - using Dan's CG trick                                        #
+####################################################################################
+
+def NLL2(params, gp1, gp2, x2d, y2d):
+    K, N = x2d.shape
+    s1 = np.append(0.0, params[:K-1])
+    x1 = (x2d + s1[:, None]).flatten()
+    inds1 = np.argsort(x1)
+    x1 = x1[inds1]
+    s2 = np.append(0.0, params[K-1:])
+    x2 = (x2d + s2[:, None]).flatten()
+    inds2 = np.argsort(x2)
+    x2 = x2[inds2]
+    # Define a custom "LinearOperator"
+    def matvec(v):
+        res = np.empty_like(v)
+        res[inds1] = gp1.dot(v[inds1], x1, check_sorted=False)[:, 0]
+        res[inds2] += gp2.dot(v[inds2], x2, check_sorted=False)[:, 0]
+        return res
+    op = LinearOperator((K*N, K*N), matvec=matvec)
+    # Solve the system and compute the first term of the log likelihood
+    y1 = y2d.flatten()
+    soln = cg(op, y1, tol=0.1)
+    value = 0.5 * np.dot(y1, soln[0])
+    return value
+
+def LP2(p, gp, x2d, y2d):
+    return -NLL2(p, gp1, gp2, x2d, y2d)
+
+def Fit2(x2d, y2d, y2derr, HPs, verbose = True):
+    K = x2d.shape[0]
+    k = terms.Matern32Term(log_sigma = HPs[0], log_rho = HPs[1])
+    k += terms.JitterTerm(log_sigma = HPs[2])
+    gp1 = GP(k, mean = 1.0)
+    gp2 = GP(k, mean = 1.0)
+    par_in = np.zeros(2*(K-1))
+    soln = minimize(NLL2, par_in, args=(gp1, gp2, x2d, y2d))
+    if verbose:
+        print 'Initial pars:', par_in
+        print 'Fitted pars:', soln.x
+    return soln.x
+
+# def Pred2_2D(par, x2d, y2d, y2derr, doPlot = True, x2dpred = None):
+#     K = x2d.shape[0]
+#     k = terms.Matern32Term(log_sigma = par[-3], log_rho = par[-2])
+#     k += terms.JitterTerm(log_sigma = par[-1])
+#     gp1 = GP(k, mean = 1.0)
+#     gp2 = GP(k, mean = 1.0)
+#     shifts = np.append(0,par[:K-1]) 
+#     x1d = (x2d + shifts[:, None]).flatten()
+#     inds = np.argsort(x1d)
+#     y1d = y2d.flatten()
+#     y1derr = y2derr.flatten()
+#     gp.compute(x1d[inds], yerr = y1derr[inds])
+#     if x2dpred is None:
+#         x2dpred = np.copy(x2d)
+#     x1dpred = (x2dpred + shifts[:, None]).flatten()
+#     indspred = np.argsort(x1dpred)
+#     mu, var = gp.predict(y1d[inds], x1dpred[indspred], return_var = True)
+#     std = np.sqrt(var)
+#     y1dpred = np.zeros_like(x1dpred)
+#     y1dpred[indspred] = mu
+#     y1dprederr = np.zeros_like(x1dpred)
+#     y1dprederr[indspred] = std
+#     y2dpred = y1dpred.reshape(x2dpred.shape)
+#     y2dprederr = y1dprederr.reshape(x2dpred.shape)    
+#     if doPlot:
+#         for i in range(K):
+#             plt.errorbar(x2d[i,:], y2d[i,:] - i, yerr = y2derr[i,:], fmt = ".k", capsize = 0, alpha = 0.5)
+#             plt.plot(x2dpred[i,:], y2dpred[i,:] - i, 'C0')
+#             plt.fill_between(x2dpred[i,:], y2dpred[i,:] + y2dprederr[i,:] - i, \
+#                                  y2dpred[i,:] - y2dprederr[i,:] - i, color = 'C0', alpha = 0.4, lw = 0)
+#     return x2dpred, y2dpred, y2dprederr
+
+def GPSpec_2Comp(wav, flux, flux_err, nsteps = 2000, nrange = 3, prefix = 'RR1'):
+    # NB: input wavelengths should be in nm, flux continuum should be about 1
+    K, N = wav.shape
+    # Create 2-D array of scaled log wavelengths for fitting
+    lwav = np.log(wav * 1e-9) # in m
+    lw0, lw1 = lwav.min(), lwav.max()
+    x = (lwav - lw0) / (lw1 - lw0)
+    # First do GP fit to individual spectra to get estimate of GP HPs
+    print 'GP fit to individual spectra'
+    HPs = np.zeros((K,3))
+    for i in range(K):
+        xx = x[i,:].flatten()
+        yy = flux[i,:].flatten()
+        ee = flux_err[i,:].flatten()
+        HPs[i,:] = Fit0_Jitter(xx, yy, ee, verbose = False)
+    HPs = np.median(HPs, axis=0)
+    print 'GP HPs:', HPs
+    # Initial (ML) estimate of parameters
+    print "Starting ML fit"
+    ML_par = np.array(Fit2(x, flux, flux_err, HPs, verbose = False))
+    par_ML = np.copy(ML_par)
+    par_ML[:2*(K-1)] *= (lw1 - lw0) * SPEED_OF_LIGHT * 1e-3
+    par_ML[-2] *= (lw1 - lw0)
+    return par_ML
+    # k = terms.Matern32Term(log_sigma = ML_par[-2], log_rho = ML_par[-1])
+    # gp = GP(k, mean = 1.0)
+    # print "ML fit done"
+    # # MCMC
+    # print "Starting MCMC"
+    # ndim = K+1
+    # nwalkers = ndim * 4
+    # p0 = ML_par + 1e-4 * np.random.randn(nwalkers, ndim)
+    # sampler = emcee.EnsembleSampler(nwalkers, ndim, LP2,
+    #                                     args = [gp, x, flux, flux_err])
+    # for i, result in enumerate(sampler.sample(p0, iterations=nsteps)):
+    #     n = int((30+1) * float(i) / nsteps)
+    #     sys.stdout.write("\r[{0}{1}]".format('#' * n, ' ' * (30 - n)))
+    # sys.stdout.write("\n")
+    # print("MCMC done")
+    # # find MAP parameters
+    # iMAP = np.argmax(sampler.flatlnprobability)
+    # MAP_par = sampler.flatchain[iMAP,:].flatten()
+    # # extract MCMC chains
+    # samples = sampler.chain
+    # Lprob = sampler.lnprobability    
+    # # convert chains back to physical units: shifts in km/s
+    # samples_tpl = np.copy(samples)
+    # samples_tpl[:,:,:K-1] *= (lw1 - lw0) * SPEED_OF_LIGHT * 1e-3
+    # samples_tpl[:,:,-1] *= (lw1 - lw0)
+    # par_MAP = np.copy(MAP_par)
+    # par_MAP[:K-1] *= (lw1 - lw0) * SPEED_OF_LIGHT * 1e-3
+    # par_MAP[-1] *= (lw1 - lw0)
+    # # parameter names for plots
+    # labels = []
+    # for i in range(K-1):
+    #     labels.append(r'$\delta v_{%d}$ (km/s)' % (i+1))
+    # labels.append(r'$\ln \sigma$')
+    # labels.append(r'$\ln \rho$')
+    # labels = np.array(labels)
+    # names = []
+    # for i in range(K-1):
+    #     names.append('dv_%d (km/s)' % (i+1))
+    # names.append('ln(sig)')
+    # names.append('ln(rho)')
+    # names = np.array(names)
+    # # Plot the chains
+    # fig1 = plt.figure(figsize = (12,K+3))
+    # gs1 = gridspec.GridSpec(ndim+1,1)
+    # gs1.update(left=0.1, right=0.98, bottom = 0.07, top = 0.98, hspace=0)
+    # ax1 = plt.subplot(gs1[0,0])    
+    # plt.setp(ax1.get_xticklabels(), visible=False)
+    # plt.plot(Lprob.T, 'k-', alpha = 0.2)
+    # plt.ylabel(r'$\ln P$')
+    # for i in range(ndim):
+    #     axc = plt.subplot(gs1[i+1,0], sharex = ax1)    
+    #     if i < (ndim-1):
+    #         plt.setp(axc.get_xticklabels(), visible=False)
+    #     plt.plot(samples_tpl[:,:,i].T, 'k-', alpha = 0.2)
+    #     plt.ylabel(labels[i])
+    # plt.xlim(0,nsteps)
+    # plt.xlabel('iteration number')
+    # # Discard burnout
+    # nburn = int(raw_input('Enter no. steps to discard as burnout: '))
+    # plt.axvline(nburn)
+    # # Evaluate and print the parameter ranges
+    # print '\n{:20s}: {:10s} {:10s} {:10s} - {:7s} + {:7s}'.format('Parameter', 'ML', 'MAP', \
+    #                                                                 'Median','Error','Error')
+    # par50 = np.zeros(ndim)
+    # par84 = np.zeros(ndim)
+    # par16 = np.zeros(ndim)
+    # for i in range(ndim):
+    #     sam = samples_tpl[:,:,i].flatten()
+    #     b, m, f = np.percentile(sam, [16,50,84])
+    #     par50[i] = m
+    #     par16[i] = b
+    #     par84[i] = f
+    #     print '{:20s}: {:10.5f} {:10.5f} {:10.5f} - {:7.5f} + {:7.5f}'.format(names[i], \
+    #                                                                               par_ML[i], \
+    #                                                                               par_MAP[i], \
+    #                                                                               m, m-b, f-m)
+    # if prefix is None:
+    #     return par_MAP, par50, par50-par16, par84-par50
+    # plt.savefig('%s_chains.png' % prefix)
+    # samples_flat = samples[:,nburn:,:].reshape(-1, ndim)
+    # samples_tpl_flat = samples_tpl[:,nburn:,:].reshape(-1, ndim)
+    # # Plot the parameter distributions
+    # fig2 = corner.corner(samples_tpl_flat, truths = par_MAP, labels = labels, show_titles = True, \
+    #                         quantiles = [0.16, 0.84])
+    # plt.savefig('%s_corner.png' % prefix)
+    # # Plot the individual spectra with MAP fit
+    # xpred, fpred, fpred_err = Pred1_2D(MAP_par, x, flux, flux_err, doPlot = False)
+    # lwpred = (lw1-lw0) * xpred + lw0
+    # wpred = np.exp(lwpred) * 1e9
+    # fig3 = plt.figure(figsize = (12,K+1))
+    # gs3 = gridspec.GridSpec(K,1)
+    # gs3.update(left=0.1, right=0.98, bottom = 0.07, top = 0.98, hspace=0)
+    # for i in range(K):
+    #     if i == 0:
+    #         ax1 = plt.subplot(gs3[0,0])
+    #     else:
+    #         axc = plt.subplot(gs3[i,0], sharex=ax1, sharey=ax1)
+    #     if i < (K-1):
+    #         plt.setp(ax1.get_xticklabels(), visible=False)
+    #     plt.errorbar(wav[i,:], flux[i,:], yerr = flux_err[i,:], \
+    #                      fmt = ".k", ms = 2, mec = 'none', capsize = 0, alpha = 0.5)
+    #     plt.plot(wpred[i,:], fpred[i,:], 'C0')
+    #     plt.fill_between(wpred[i,:], fpred[i,:] + 2 * fpred_err[i,:], \
+    #                          fpred[i,:] - fpred_err[i,:], color = 'C0', alpha = 0.4, lw = 0)
+    #     plt.ylabel('spec. %d' % (i+1))
+    # plt.xlim(wav.min(), wav.max())
+    # plt.xlabel('wavelength (nm)')
+    # plt.savefig('%s_spectra.png' % prefix)
+    # # Plot the combined spectra with samples from MCMC chain
+    # shifts = np.append(0,MAP_par[:K-1])
+    # x1d = (x + shifts[:, None]).flatten()
+    # lw1d = (lw1-lw0) * x1d + lw0
+    # w1d = np.exp(lw1d) * 1e9
+    # y1d = flux.flatten()
+    # y1derr = flux_err.flatten()
+    # inds = np.argsort(x1d)
+    # gp.set_parameter_vector(MAP_par[-2:])
+    # gp.compute(x1d[inds], yerr = y1derr[inds])
+    # fig4 = plt.figure(figsize = (12,nrange+1))
+    # gs4 = gridspec.GridSpec(nrange,1)
+    # gs4.update(left=0.1, right=0.98, bottom = 0.07, top = 0.98, hspace=0.05)
+    # ws = w1d.min()
+    # wr = (w1d.max()-ws) / float(nrange)
+    # for i in range(nrange):
+    #     if i == 0:
+    #         ax1 = plt.subplot(gs4[0,0])
+    #     else:
+    #         axc = plt.subplot(gs4[i,0], sharey=ax1)
+    #     if i < (nrange-1):
+    #         plt.setp(ax1.get_xticklabels(), visible=False)
+    #     wmin = ws + (i - 0.05) * wr
+    #     wmax = ws + (i + 1.05) * wr
+    #     l = (w1d >= wmin) * (w1d <= wmax)
+    #     plt.errorbar(w1d[l], y1d[l], yerr = y1derr[l], fmt = ".k", capsize = 0, \
+    #                      alpha = 0.5, ms = 2, mec='none')
+    #     wpred = np.linspace(wmin, wmax, 1000)
+    #     lwpred = np.log(wpred * 1e-9)
+    #     xpred = (lwpred-lw0)/(lw1-lw0)
+    #     isamp = np.random.randint(nsteps-nburn, size=10)
+    #     for j in isamp:
+    #         samp_params = samples_flat[j,:].flatten()
+    #         samp_shifts = np.append(0, samp_params[:K-1])
+    #         x1_samp = (x + samp_shifts[:, None]).flatten()
+    #         inds_samp = np.argsort(x1_samp)
+    #         k_samp = terms.Matern32Term(log_sigma=samp_params[-2],log_rho=samp_params[-1])
+    #         gp_samp = GP(k_samp, mean=1.)
+    #         gp_samp.compute(x1_samp[inds_samp], yerr = y1derr[inds_samp])
+    #         mu, _ = gp.predict(y1d[inds_samp], xpred, return_var = True)
+    #         plt.plot(wpred, mu, 'C0-', lw = 0.5, alpha = 0.5)
+    #     plt.xlim(wmin, wmax)
+    #     plt.ylabel('flux')
+    # plt.xlabel('wavelength (nm)')
+    # plt.savefig('%s_combined.png' % prefix)
+    # return par_MAP, par50, par50-par16, par84-par50, [fig1, fig2, fig3, fig4]
+    
+def test2():
+    plt.close('all')
+    d = siom.loadmat('/Users/aigrain/Soft/GPSpec/data/synth_dataset_003.mat')
+    wav = d['wavelength'] * 0.1
+    K,N = wav.shape
+    flux = d['flux']
+    flux_err = d['error']
+    baryvel = d['baryvel'].flatten()
+    starvel = d['starvel']
+    shifts = np.zeros(2*(K-1))
+    s1 = baryvel + starvel[:,0].flatten()
+    shifts[:K-1] = s1[1:]-s1[0]
+    s2 = baryvel + starvel[:,1].flatten()
+    shifts[K-1:] = s2[1:]-s1[0]
+    print shifts / 1e3
+    res = GPSpec_2Comp(wav, flux, flux_err)
+    print res
+    return
+
+####################################################################################
+# The code below is my crappy implementation of Vinesh's approach for the single   #
+# component case. However, it doesn't quite work as expected and I've not pursued  #
+# it further.                                                                      #
+####################################################################################
+
+
+def pairwise_ccf(spec, maxlags = None):
+    K,N = spec.shape
+    lags = np.arange(-N+1,N)
+    ccfs = np.zeros((K,K,2*N-1))
+    for i in np.arange(K):
+        x = spec[i,:].flatten()
+        for j in np.arange(K):
+            if j > i:
+                continue
+            print i, j
+            y = spec[j,:].flatten()
+            ccfs[i,j,:] = np.correlate(x, y, 'full') / np.sqrt(np.dot(x, x) * np.dot(y, y))
+    if maxlags:
+        l = abs(lags) <= maxlags
+        lags = lags[l]
+        ccfs = ccfs[:,:,l]
+    return lags, ccfs
+
+def Wolkswagen(x2d, y2d, y2derr, truevel, doLog = True, sampling = 100, wav2si = 1.0e-10):
+    K, N = x2d.shape
+    if doLog:
+        xang = x2d * wav2si
+        log_xpred = np.linspace(np.log(xang.min()), np.log(xang.max()), N*sampling)
+        xpred = np.exp(log_xpred) / wav2si
+        dlw = log_xpred[1]-log_xpred[0]
+    else:
+        xpred = np.linspace(x2d.min(), x2d.max(), N*sampling)
+        dw = xpred[1]-xpred[0]
+    HPs = np.zeros((K,2))
+    ypred = np.zeros((K,len(xpred)))
+    plt.figure(1)
+    plt.clf()
+    for i in range(K):
+        x = x2d[i,:].flatten()
+        y = y2d[i,:].flatten()
+        yerr = y2derr[i,:].flatten()
+        plt.errorbar(x, y - i, yerr = yerr, fmt = 'k.', capsize = 0, alpha=0.5)
+        res = Fit0(x, y, yerr, verbose = False, xpred = xpred)
+        HPs[i,:] = res[0]
+        ypred[i,:] = res[1]
+        plt.plot(xpred, res[1] - i, color='C0')
+        plt.fill_between(xpred, res[1] + res[2] - i, res[1] - res[2] - i, \
+                             color = 'C0', alpha=0.4, lw=0)
+    lags, ccfs = pairwise_ccf(ypred, 500)
+    dvel = lags * dlw * 2.99796458e8 / 1e3
+    plt.figure(2)
+    plt.clf()
+    for i in range(K):
+        for j in range(K):
+            if j > i: continue
+            plt.clf()
+            plt.plot(dvel, ccfs[i,j,:].flatten())
+            ipk = np.argmax(ccfs[i,j,:].flatten())
+            plt.axvline(dvel[ipk])
+            print i,j, dvel[ipk], (truevel[j]-truevel[i])/1e3
+            plt.draw()
+            raw_input()
+
--- soft/simulate_dataset_2stars.py
+++ soft/simulate_dataset_2stars.py
@@ -3,6 +3,8 @@ import pylab as pl
 import scipy.io as sio
 from astropy.convolution import convolve, Gaussian1DKernel
 
+ncomp = 2
+
 SPEED_OF_LIGHT = 2.99796458e8
 nsim = 5
 wmin = 5000 # Angstrom
@@ -10,54 +12,59 @@ wmax = 6000
 npix = 10000
 resol = 10 # 10 pix = 1 Angstrom
 
+np.random.seed(1234)
+
 ##############################
 # Simulate synthetic dataset #
 ##############################
 
 dwav = (wmax - wmin) / float(npix)
 wav = np.arange(npix) * dwav + wmin
+wav_earth = np.tile(wav, (nsim, 1))
+lwav = np.log(wav * 1e-10)
 
 def gauss1(x,a,mu,sig):
     y = a * np.exp(-(x-mu)**2/2/sig**2)
     return y
 
-# Define properties of spectral lines
-nlines = 100
-np.random.seed(2)
-m = np.random.uniform(wmin, wmax,nlines)
-s = 10.0**np.random.uniform(-1,0.5,nlines)
-# even the narrowest lines are well sampled
-a = 0.05*10.0**np.random.uniform(0,1,nlines)
 
 # Define "barycentric" velocity shifts (chose those so as to span most
 # of a pixel)
 baryvel_max = SPEED_OF_LIGHT * 2 / (wmax + wmin)
 baryvel = np.random.uniform(high = baryvel_max, size = nsim)
 print baryvel
-# Define additional intrinsic velocity shifts of up to 200 km/s
-starvel = np.append(0, np.sort(np.random.uniform(high = 200.0e3, size=nsim-1)))
-print starvel
 
-lwav = np.log(wav * 1e-10)
-dlw = (baryvel + starvel) / SPEED_OF_LIGHT
-print dlw
-lwav_rest = np.zeros((nsim, npix))
-wav_rest = np.zeros((nsim, npix))
-for i in np.arange(nsim):
-    lwav_rest[i,:] = lwav + dlw[i]
-wav_rest = np.exp(lwav_rest) * 1e10
-wav_earth = np.tile(wav, (nsim, 1))
-
-# Simulate emitted spectra
+starvel = np.zeros((nsim, ncomp))
 flux = np.ones((nsim, npix))
-for i in np.arange(nsim):
-    for j in np.arange(nlines):
-        flux[i,:] -= gauss1(wav_rest[i,:], a[j], m[j], s[j])
-pl.close('all')
-pl.figure(figsize = (8,6))
-ax1 = pl.subplot(311)
-pl.plot(wav_rest.T, flux.T, 'k,')
-pl.ylabel('true')
+# Define generic properties of spectral lines
+nlines = 10
+m = np.random.uniform(wmin, wmax,nlines)
+s = 10.0**np.random.uniform(-1,0.5,nlines)
+# even the narrowest lines are well sampled
+a = 0.05*10.0**np.random.uniform(0,1,nlines)
+fac = 0.5
+
+for c in range(ncomp):
+
+    # Define intrinsic velocity shifts of up to 2000 km/s
+    starvel[:,c] = np.random.uniform(high = 2000.0e3, size=nsim)
+    print starvel[:,c]
+
+    dlw = (baryvel + starvel[:,c].flatten()) / SPEED_OF_LIGHT
+    lwav_rest = np.zeros((nsim, npix))
+    for i in np.arange(nsim):
+        lwav_rest[i,:] = lwav + dlw[i]
+    wav_rest = np.exp(lwav_rest) * 1e10
+
+    if c>0:
+        # for each new component the lines get shallower and broader
+        a *= fac
+        s /= fac
+        
+    # Simulate emitted spectra
+    for i in np.arange(nsim):
+        for j in np.arange(nlines):
+            flux[i,:] -= gauss1(wav_rest[i,:], a[j], m[j], s[j])
 
 # Degrade resolution
 flux_deg = np.copy(flux)
@@ -66,6 +73,7 @@ for i in np.arange(nsim):
 # get rid of the edges which are screwed up by the convolution
 wav_deg = wav_earth[:,40:-40]
 flux_deg = flux_deg[:,40:-40]
+
 # Resample to Nyquist sampling
 for i in np.arange(nsim):
     if i == 0:
@@ -77,10 +85,6 @@ for i in np.arange(nsim):
     else:
         wav_obs[i,:] = wav_deg[i,::resol]
     flux_obs[i,:] = flux_deg[i,::resol]
-ax2 = pl.subplot(312, sharex = ax1, sharey = ax1)
-pl.plot(wav_deg.T, flux_deg.T, 'k-')
-pl.plot(wav_obs.T, flux_obs.T, '.')
-pl.ylabel('degraded')
 
 # Add noise
 sigma = 0.01 * np.sqrt(flux_obs)
@@ -88,9 +92,11 @@ flux_noisy = np.copy(flux_obs)
 for i in np.arange(nsim):
     noise = np.random.normal(0,1,npix_new) * sigma[i,:]
     flux_noisy[i,:] += noise
-ax3 = pl.subplot(313, sharex = ax1, sharey = ax1)
-pl.plot(wav_obs.T, flux_noisy.T, '.')
-pl.ylabel('noisy')
+
+pl.clf()
+for i in range(nsim):
+    pl.plot(wav_obs[i,:], flux_noisy[i,:]-i, '.')
+pl.ylabel('flux')
 pl.xlim(wmin, wmax)
 pl.xlabel('wavelength (Angstrom')
 pl.savefig('../plots/synth_dataset_003.png')
